829dfaa7559bed7c6f26280a32c71cecff09bf7a
David Mendez <dmendez15@islander.tamucc.edu>
2021-04-18 16:13:37 -0500

    Created Tables to insert flash card data into

--- LogInWebform.aspx.cs
+++ LogInWebform.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace QuickStudyWebsite
 {
@@ -12,6 +14,33 @@ namespace QuickStudyWebsite
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            Page.Validate();
+            if (Page.IsValid)
+            {
+                using (SqlConnection sqlCon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\UserDatabase.mdf;Integrated Security=True"))
+                {
+                    sqlCon.Open();
+                    string query = "select count(1) from Users where User_UserName=@username and User_Password=@password";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@username", txtboxUserName.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@password", txtboxPassword.Text.Trim());
+                    int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                    if (count == 1)
+                    {
+                        Session["username"] = txtboxUserName.Text.Trim();
+                        //Todo add a Response.Redirect("Index.aspx");
+                        // To have a user Log Out, place these next 2 lines on a log out button click:
+                        // Session.Abandon();
+                        // Response.Redirect("LogInWebform.aspx");
+                    }
+                    
+                    sqlCon.Close();
+                }
+            }
         }
     }
 }
--- SignUpWebform.aspx.cs
+++ SignUpWebform.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace QuickStudyWebsite
 {
@@ -12,6 +14,40 @@ namespace QuickStudyWebsite
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnCreateAccount_Click(object sender, EventArgs e)
+        {
+            Page.Validate();
+            if (Page.IsValid)
+            {
+                using (SqlConnection sqlCon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\UserDatabase.mdf;Integrated Security=True"))
+                {
+                    sqlCon.Open();
+                    string query = "select count(1) from Users where User_UserName=@username or User_Email=@email";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@username", txtboxUserName.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@email", txtboxEmail.Text.Trim());
+                    int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+
+                    if (count >= 1)
+                    {
+                        Response.Write("Username or Email already exists.");
+                    }
+                    else
+                    {
+                        string insert = "insert into Users (User_UserName,User_Email,User_Password) values (@username, @email, @password)";
+                        SqlCommand cmd = new SqlCommand(insert, sqlCon);
+                        cmd.Parameters.AddWithValue("@username", txtboxUserName.Text.Trim());
+                        cmd.Parameters.AddWithValue("@email", txtboxEmail.Text.Trim());
+                        cmd.Parameters.AddWithValue("@password", txtboxPassword.Text.Trim());
+
+                        cmd.ExecuteNonQuery();
+                        Response.Redirect("LogInWebform.aspx");
+                    }
+                    sqlCon.Close();
+                }
+            }
         }
     }
 }
