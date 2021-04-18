7b702fde9b31cfcb7fd36e1406c98dde458948b8
David Mendez <dmendez15@islander.tamucc.edu>
2021-04-17 19:40:42 -0500

    Added labels that show what card you're on.

--- js/FlashCard.js
+++ js/FlashCard.js
@@ -2,14 +2,16 @@
 var DeckName = [];
 var cardText = document.getElementById("cardText");
 var deckTitle = document.getElementById("deckTitle");
+var currentSide = document.getElementById("currentSide");
+var cardNumber = 1;
 var deckIndex = 0;
 var cardIndex = 0;
 var showingA = true;
 
 var newCard = ["", ""]; // A blank card used for newly created decks. Get's overridden in addCard() function.
 
-var card1 = ["Byte", "A group of binary digits or bits (usually eight) operated on as a unit."];
-var card2 = ["HTML", "Hypertext Markup Language, a standardized system for tagging text files to achieve font, color, graphic, and hyperlink effects on World Wide Web pages."];
+var card1 = ["HTML", "Hypertext Markup Language, a standardized system for tagging text files to achieve font, color, graphic, and hyperlink effects on World Wide Web pages."];
+var card2 = ["Byte", "A group of binary digits or bits (usually eight) operated on as a unit."];
 var card3 = ["CSS", "Stands for Cascading Style Sheet."];
 
 Deck[0] = [card1, card2, card3];
@@ -17,16 +19,20 @@ DeckName[0] = "Internet Programming";
 
 cardText.innerHTML = Deck[deckIndex][cardIndex][0];
 deckTitle.innerHTML = DeckName[0];
+updateSideTracker(); // Updates a label that says what side the card is facing.
+updateCardCounter(); // Updates a label that shows what the current card you are at is.
 
 //Flip button
 function flipCard() {
     if (showingA) {
         cardText.innerHTML = Deck[deckIndex][cardIndex][1];
         showingA = false;
+        updateSideTracker();
     }
     else {
         cardText.innerHTML = Deck[deckIndex][cardIndex][0];
         showingA = true;
+        updateSideTracker();
     }
 }
 
@@ -40,6 +46,7 @@ function addCard() {
             Deck[deckIndex][0] = [sideA, sideB];
             cardText.innerHTML = Deck[deckIndex][0][0]; // Show sideA of the brand new card.
             showingA = true;
+            updateSideTracker();
         }
         else {
             Deck[deckIndex].push([sideA, sideB]);
@@ -50,19 +57,23 @@ function addCard() {
     else {
         alert('Both sides of the card must be filled');
     }
-    
+    updateCardCounter();
 }
 
 function deleteCard() {
     Deck[deckIndex].splice(cardIndex, 1);
     cardText.innerHTML = "";
     prevCard();
+    updateCardCounter();
 }
 
 function nextCard() {
     cardIndex = (cardIndex + 1) % Deck[deckIndex].length;
     cardText.innerHTML = Deck[deckIndex][cardIndex][0];
+    cardNumber.innerHTML = cardIndex;
     showingA = true;
+    updateSideTracker();
+    updateCardCounter();
 }
 
 function prevCard() {
@@ -72,8 +83,11 @@ function prevCard() {
     else if (cardIndex == 0) {
         cardIndex = Deck[deckIndex].length - 1;
     }
+    cardNumber.innerHTML = cardIndex;
     cardText.innerHTML = Deck[deckIndex][cardIndex][0];
     showingA = true;
+    updateSideTracker();
+    updateCardCounter();
 }
 
 function randomCard() {
@@ -84,8 +98,11 @@ function randomCard() {
     else {
         cardIndex = randomNum;
     }
+    cardNumber.innerHTML = cardIndex;
     cardText.innerHTML = Deck[deckIndex][cardIndex][0];
     showingA = true;
+    updateSideTracker();
+    updateCardCounter();
 }
 
 //Deck Functions
@@ -94,6 +111,9 @@ function nextDeck() {
     deckIndex = (deckIndex + 1) % Deck.length;
     deckTitle.innerHTML = DeckName[deckIndex];
     cardText.innerHTML = Deck[deckIndex][0][0];
+    showingA = true;
+    updateSideTracker();
+    updateCardCounter();
 }
 
 function prevDeck() {
@@ -105,6 +125,9 @@ function prevDeck() {
     }
     deckTitle.innerHTML = DeckName[deckIndex];
     cardText.innerHTML = Deck[deckIndex][0][0];
+    showingA = true;
+    updateSideTracker();
+    updateCardCounter();
 }
 
 //When the user clicks the "Create new deck" button
@@ -122,6 +145,7 @@ function newDeck() {
         DeckName[DeckName.length] = newDeckName;
         closeNameWindow();
     }
+    updateCardCounter();
 }
 
 //Delete current deck
@@ -129,4 +153,18 @@ function deleteDeck() {
     Deck.splice(deckIndex, 1);
     DeckName.splice(deckIndex, 1);
     prevDeck();
+    updateCardCounter();
+}
+
+function updateCardCounter() {
+    cardNumber = cardIndex + 1;
+    document.getElementById("cardNumber").innerHTML = "Card " + cardNumber + "/" + Deck[deckIndex].length;
+}
+function updateSideTracker() {
+    if (showingA) {
+        currentSide.innerHTML = "Front";
+    }
+    else {
+        currentSide.innerHTML = "Back";
+    }
 }
